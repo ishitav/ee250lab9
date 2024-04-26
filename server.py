40246af28545a1bec751cc53af3bf8c7377053de
Ishita Vasishtha <ivasisht@usc.edu>
2024-04-26 15:38:01 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from main import process1, process2  # Assuming process2 is imported as well.
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'Welcome'})
+
+@app.route('/process1', methods=['POST'])
+def process1_route():
+    """Process the data using process1."""
+    data = request.get_json()
+    return jsonify(process1(data))
+
+@app.route('/process2', methods=['POST'])
+def process2_route():
+    """Process the data using process2."""
+    data = request.get_json()
+    return jsonify(process2(data))
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5001)
